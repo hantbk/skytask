a578bbe418a98e38f427ef3b312214bf32615f7a
Nguyen Van Giang <giang16012405@gmail.com>
2024-10-23 21:35:56 +0700

    fix refactor project structure

--- frontend/src/pages/Boards/_id.jsx
+++ frontend/src/pages/Boards/_id.jsx
@@ -1,18 +1,16 @@
+import Container from '@mui/material/Container'
+import AppBar from '~/components/AppBar'
+import BoardBar from './BoardBar'
+import BoardContent from './BoardContent'
 
-import Container from '@mui/material/Container';
-import ModeSelect from '../../components/ModeSelect';
-import AppBar from '../../components/AppBar';
-import BoardBar from './BoardBar';
-import BoardContent from './BoardContent';
-
-function Boards() {
-    return (
-        <Container disableGutters maxWidth={false} sx={{ height: '100vh', backgroundColor: 'primary.main' }}>
-          <AppBar />
-          <BoardBar />
-          <BoardContent />
-        </Container>
-      )
+function Board() {
+  return (
+    <Container maxWidth={false} sx={{ height: '100vh' }}>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
+    </Container>
+  )
 }
 
-export default Boards
+export default Board
