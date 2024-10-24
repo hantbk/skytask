c3669c7a77e9ec75af49215986f576e837152f4e
Ha Nguyen <captainnemot1k60@gmail.com>
2024-10-24 14:47:16 +0700

    Refactor board routes and configuration, and add createNew endpoint
    - Refactored the board routes and configuration files to improve code organization and readability.
    - Added a new endpoint, createNew, to handle the creation of a new board.
    - The createNew endpoint returns a success message upon successful creation and handles internal server errors.

--- backend/src/validations/generator.js
+++ backend/src/validations/generator.js
@@ -19,6 +19,7 @@ export function validationGenerator(correctCondition, target = 'body') {
       const dataToValidate = req[target]
       // abortEarly: false if has many validation errors will return all errors
       await correctCondition.validateAsync(dataToValidate, { abortEarly: false })
+      // Validate successful -> Controller
       next()
     } catch (error) {
       next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, error.message))
