30fef6d9bb0f25e891e92d543b5ce6495d542a34
Ha Nguyen <captainnemot1k60@gmail.com>
2024-11-06 00:28:21 +0700

    Refactor Board component to use fetched board data instead of mock data

--- frontend/src/pages/Boards/_id.jsx
+++ frontend/src/pages/Boards/_id.jsx
@@ -1,31 +1,31 @@
-// import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import { mockData } from '~/apis/mock-data'
-// import { fetchBoardDetailsAPI } from '~/apis'
+import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
-  // const [board, setBoard] = useState(null)
+  const [board, setBoard] = useState(null)
 
-  // useEffect(() => {
-  //   // Tạm thời fix cứng boardId
-  //   // Sau (khoá advanced): Sử dụng react-router-dom để lấy boardId từ URL
-  //   const boardId = '671f0baf68e260189a05c45b'
+  useEffect(() => {
+    // Tạm thời fix cứng boardId
+    // Sau (khoá advanced): Sử dụng react-router-dom để lấy boardId từ URL
+    const boardId = '672a464c2caf73074eb27d50'
 
-  //   // Call API
-  //   fetchBoardDetailsAPI(boardId)
-  //     .then(board => {
-  //       setBoard(board)
-  //     })
-  // }, [])
+    // Call API
+    fetchBoardDetailsAPI(boardId)
+      .then(board => {
+        setBoard(board)
+      })
+  }, [])
 
   return (
     <Container maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData.board} />
-      <BoardContent board={mockData.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
     </Container>
   )
 }
