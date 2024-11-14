8980c66fdb1e9f17044e88a2e374486922b58db4
Ha Nguyen <captainnemot1k60@gmail.com>
2024-11-14 08:57:38 +0700

    add environment configuration and update build scripts for improved flexibility

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,1 +1,13 @@
-export const API_ROOT = 'http://localhost:8017'
+import { env } from '~/config/environment'
+
+export const API_ROOT = env.BUILD_MODE === 'dev'
+  ? env.DEV_API_ROOT
+  : env.PROD_API_ROOT
+
+export const DEFAULT_PAGE = 1
+export const DEFAULT_ITEMS_PER_PAGE = 12
+
+export const CARD_MEMBER_ACTIONS = {
+  ADD: 'ADD',
+  REMOVE: 'REMOVE'
+}
